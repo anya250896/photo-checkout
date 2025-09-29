ba383c42a67a779cb3ebe41dc696b0571f0cd2b8
anya250896 <anya250896@gmail.com>
2025-09-29 21:42:57 +0200

    Delete cancel.js
    Удаляем лишний cancel.js

--- cancel.js
+++
@@ -1,8 +0,0 @@
-export default function Cancel() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial, sans-serif' }}>
-      <h1>Płatność anulowana ❌</h1>
-      <p>Możesz spróbować ponownie lub wrócić na stronę główną.</p>
-    </div>
-  );
-}
