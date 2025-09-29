837257b0b3732f19f421ce4b294c5b385197ad69
anya250896 <anya250896@gmail.com>
2025-09-29 21:15:22 +0200

    Create success.js

---
+++ pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Dziękuję za zakup! 🎉</h1>
+      <p>Sprawdź swoją pocztę – wysłałam tam link do pobrania zdjęć.</p>
+    </div>
+  );
+}
